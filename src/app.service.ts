0cb788ea6430c09ba6d51d7af43f4d524d61e7ef
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-03-24 12:47:17 +0100

    Conexión como inyectable y ejecutando un SELECT

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,30 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
 import config from './config';
 
 @Injectable()
 export class AppService {
 	constructor(
+		@Inject('PG') private clientPg: Client,
 		@Inject('TASKS') private tasks: any[],
 		@Inject(config.KEY) private configService: ConfigType<typeof config>,
 	) {}
+
 	getHello(): string {
 		const apiKey = this.configService.apiKey;
 		const name = this.configService.database.name;
 		return `Hello World! ${apiKey} ${name}`;
+	}
+
+	getTasks() {
+		return new Promise((resolve, reject) => {
+			this.clientPg.query('SELECT * FROM tasks', (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(res.rows);
+			});
+		});
 	}
 }
