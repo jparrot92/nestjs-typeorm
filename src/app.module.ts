c59950ae13c8e2ffb42b62c4d25804ddc7bca180
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-03-24 11:42:53 +0100

    Integración de node-postgres con NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { HttpModule, HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { Client } from 'pg';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,6 +12,20 @@ import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
 import config from './config';
+
+const client = new Client({
+	user: 'root',
+	host: 'localhost',
+	database: 'my_db',
+	password: '123456',
+	port: 5432,
+});
+
+client.connect();
+client.query('SELECT * FROM tasks', (err, res) => {
+	console.error(err);
+	console.log(res.rows);
+});
 
 @Module({
 	imports: [
