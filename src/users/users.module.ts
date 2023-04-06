a6ec757f9ca339185a9a5ba98d9a2aa9fef92286
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-04-06 17:21:12 +0200

    Relaciones muchos a muchos personalizadas

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,39 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ProductsModule } from '../products/products.module';
+
 import { CustomerController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
 import { Customer } from './entities/customer.entity';
+
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { User } from './entities/user.entity';
 
-import { ProductsModule } from '../products/products.module';
+import { OrdersService } from './services/orders.service';
+import { OrdersController } from './controllers/orders.controller';
+import { Order } from './entities/order.entity';
 
+import { OrderItemController } from './controllers/order-item.controller';
+import { OrderItemService } from './services/order-item.service';
+import { OrderItem } from './entities/order-item.entity';
 @Module({
-	imports: [ProductsModule, TypeOrmModule.forFeature([User, Customer])],
-	controllers: [CustomerController, UsersController],
-	providers: [CustomersService, UsersService],
+	imports: [
+		ProductsModule,
+		TypeOrmModule.forFeature([User, Customer, Order, OrderItem]),
+	],
+	controllers: [
+		CustomerController,
+		UsersController,
+		OrdersController,
+		OrderItemController,
+	],
+	providers: [
+		CustomersService,
+		UsersService,
+		OrdersService,
+		OrderItemService,
+	],
 })
 export class UsersModule {}
