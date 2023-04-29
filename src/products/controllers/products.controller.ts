a6ff0efc11a0fc8785833b62840ebd7143ac50d0
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-04-29 13:32:26 +0200

    Extendiendo JWT Guard

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -24,19 +24,23 @@ import {
 	FilterProductsDto,
 } from '../dtos/products.dto';
 import { ProductsService } from '../services/products.service';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { Public } from '../../auth/decorators/public.decorator';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
 	constructor(private productsService: ProductsService) {}
 
+	@Public()
 	@Get()
 	@ApiOperation({ summary: 'List of products' })
 	getProducts(@Query() params: FilterProductsDto) {
 		return this.productsService.findAll(params);
 	}
 
+	@Public()
 	@Get('filter')
 	getProductFilter() {
 		return `yo soy un filter`;
